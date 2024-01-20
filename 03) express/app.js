b1d6be1cfa9c4379df2744bad8619d829d0a6f95
samir984 <sasuki984@gmail.com>
2024-01-20 20:49:47 +0545

    learn database

---
+++ 03) express/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) Middleware
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('hello from Middleware');
+  next();
+});
+
+// 3) Routers
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+//servering lisinig
+module.exports = app;
